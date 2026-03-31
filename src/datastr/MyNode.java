bad42b2544b593749bc1f6e1fdc1fe64c080cf13
Daniils <daniils.strals@gmail.com>
2026-03-31 11:04:20 +0300

    Add: added MyNode class with variables, getters, setters, constructor and toString

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,46 @@
 package datastr;
 
-public class MyNode {
+public class MyNode<Ttype> {
+	private Ttype element;
+	private MyNode<Ttype> nextNode = null;
+	private MyNode<Ttype> previousNode = null;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	
+	public MyNode<Ttype> getNextNode() {
+		return nextNode;
+	}
+	
+	public MyNode<Ttype> getPreviousNode() {
+		return previousNode;
+	}
 
+	public void setElement(Ttype element) {
+		if(element != null) {
+			this.element = element;
+		} else {
+			this.element = (Ttype) new Object();
+		}
+	}
+
+	// nav jāveic not null pārbaudes, jo var būt gadījumi, ka ir null vērtība pirmajā vai pēdējā elementā
+	public void setNextNode(MyNode<Ttype> nextNode) {
+		this.nextNode = nextNode;
+	}
+
+	public void setPreviousNode(MyNode<Ttype> previousNode) {
+		this.previousNode = previousNode;
+	}
+	
+	//bez argumenta konstruktors nav vajadzīgs, jo vienmēr lietotājam būs jānorāda elements
+	public MyNode(Ttype element) {
+		setElement(element);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
+	
 }
